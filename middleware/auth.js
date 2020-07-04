f9ae5ea9501cacf365d84d35906fb1b9aa7268f2
Your Name <prajjawal.tiwari.5@gmail.com>
2020-07-04 15:59:26 +0530

    changed auth file

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,15 +1,20 @@
 const jwt = require("jsonwebtoken");
 const config = require("config");
-module.exports = function (req, res, next) {
+module.exports = async function (req, res, next) {
   //getting token from header
   const token = req.header("x-auth-token");
   //check jwtToken
   if (!token) return res.status(401).json({ msg: "authentication error" });
   //verify jwtToken
   try {
-    const decoded = jwt.verify(token, config.get("JWTsecret"));
-    req.user = decoded.payload.user;
-    next();
+    await jwt.verify(token, config.get("JWTsecret"), (error, decoded) => {
+      if (error) {
+        res.status(401).json({ msg: "Token is not valid" });
+      } else {
+        req.user = decoded.user;
+        next();
+      }
+    });
   } catch (err) {
     console.error(err);
     res.status(500).send("server error");
